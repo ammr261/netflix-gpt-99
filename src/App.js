4a011220794d679d554c58acc899d0033c8b0d96
ammr261 <ammarrohe@gmail.com>
2024-02-27 02:54:07 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-800 " >
+      Hello Everyone
+    </div>
+  );
+}
+
+export default App;
